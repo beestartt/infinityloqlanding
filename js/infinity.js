70c22fed9fdf260f545035b53205b6543fe894bc
Rodirgo Xavier <rodrigoxavier.xavi@gmail.com>
2017-05-30 11:09:41 -0300

    Ajustando as imagens

---
+++ js/infinity.js
@@ -0,0 +1,7 @@
+$( document ).ready(function() {
+	if ($(window).width() <= 500){
+	    $("#capa-banner1").attr("src", "img/capa-banner-mobile2.jpg");
+	    $("#capa-banner2").attr("src", "img/capa-banner-mobile2.jpg");
+	    $("#capa-banner3").attr("src", "img/capa-banner-mobile3.jpg");
+	}
+});
