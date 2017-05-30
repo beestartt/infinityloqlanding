ed253ba857ff2d56e61d68e3326872acf595fe43
Rodirgo Xavier <rodrigoxavier.xavi@gmail.com>
2017-05-30 11:22:55 -0300

    Exibindo as imagens dinamicamente

--- js/infinity.js
+++ js/infinity.js
@@ -1,7 +1,16 @@
 $( document ).ready(function() {
+	
+	checkScreenSizeAndChangeImages();
+
+	$( window ).resize(function() {
+	  checkScreenSizeAndChangeImages();
+	});
+});
+
+function checkScreenSizeAndChangeImages(){
 	if ($(window).width() <= 500){
 	    $("#capa-banner1").attr("src", "img/capa-banner-mobile1.jpg");
 	    $("#capa-banner2").attr("src", "img/capa-banner-mobile2.jpg");
 	    $("#capa-banner3").attr("src", "img/capa-banner-mobile3.jpg");
 	}
-});
+}
