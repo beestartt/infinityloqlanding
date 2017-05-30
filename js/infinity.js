4776f324c764e2098d2103223239dfb281806de8
Rodirgo Xavier <rodrigoxavier.xavi@gmail.com>
2017-05-30 11:19:15 -0300

    Imagens

--- js/infinity.js
+++ js/infinity.js
@@ -1,6 +1,6 @@
 $( document ).ready(function() {
 	if ($(window).width() <= 500){
-	    $("#capa-banner1").attr("src", "img/capa-banner-mobile2.jpg");
+	    $("#capa-banner1").attr("src", "img/capa-banner-mobile1.jpg");
 	    $("#capa-banner2").attr("src", "img/capa-banner-mobile2.jpg");
 	    $("#capa-banner3").attr("src", "img/capa-banner-mobile3.jpg");
 	}
